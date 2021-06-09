ec1fc34290b56bc06c1d7ac1e563da61dd586935
Savannah Simonis <savannah.simonis@x-center.com>
2021-06-09 08:05:37 +0200

    Finished assignment math-methods.js

--- math-methods.js
+++ math-methods.js
@@ -9,17 +9,22 @@ console.log(Math.PI);
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
 
+const cirkel = 4 * Math.PI;
+console.log(cirkel);
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
 
-
+const cirkel2 = 12 * Math.PI;
+console.log(Math.round(cirkel2));
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
 
+const root = Math.sqrt(9);
+console.log(root);
 
 
 /* Opdracht 4: */
@@ -28,3 +33,15 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+
+const rand = Math.random();
+console.log(rand);
+if (rand > 0.5) {
+    console.log("Het glas is halfvol");
+}
+if (rand < 0.5) {
+    console.log("Het glas is half leeg");
+}
+else if (rand === 0.5) {
+    console.log("De opdracht houdt geen rekening met waarde 0.5 zelf :)");
+}
